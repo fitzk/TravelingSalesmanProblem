34f59be829d9fe534780e2a31ebe56c2e12e98e3
fitzsimk <kfitzsimmons11@gmail.com>
2015-08-08 09:05:22 -0700

    handles input file

--- submit/run_TSP.py
+++ submit/run_TSP.py
@@ -1,26 +1,34 @@
 from sys import argv
 import re
 
-array=[]
+array_w_identifier=[]
+array_wo_identifier=[]
 fileName = '../no_submit/tsp_example_1.txt' #change this before submit
 if len(argv) > 1:
     script, filename = argv
 
 outFile = fileName[:-4] + '.result'
 try:
-    f = open(fileName)
-    array = f.read()
+    with open(fileName) as f:
+        for line in f:
+            array_w_identifier.append([int(s) for s in line.split() if s.isdigit()])
 except IOError as e:
     print 'Error in opening file '+ fileName
     sys.exit(-1)
 
 f2 = open(outFile,'w')
 
+for each in array_w_identifier:
+    array_wo_identifier.append(each[1:])
 
-int_array=[]
-for line in array:
-    if re.findall('\d+\',line)
-        
-for intz in int_array:
-    print intz
+for each in array_wo_identifier:
+    print each
+for each in array_wo_identifier:
+    print each
+
+# file reads in char by char
+# need to figure out best way to group numbers
+# and how the algorithms should take input
+
+
 f2.close()
