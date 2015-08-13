598c744a56e1d7a908a9b1203c04b4b4c9c927e7
Baljot Singh <baljotsingh1986@yahoo.com>
2015-08-12 20:54:15 -0700

    Update k--opt.py

--- k--opt.py
+++ k--opt.py
@@ -41,16 +41,60 @@ def greedy_tsp(cities):
     MSTedges = list()
    
     keyy = {}
-    finalPath = []
     parent = {}
     for k in cities:
-        finalPath.append(k)
+        u = k
+        break
+    for v in cities:
+        keyy[v] = calculate_distance(cities[u], cities[v])
+        parent[v] = u
+   
+    for i in range(0, len(cities) - 1):
+        minimum = sys.maxint
+        vertex = None
+        for v in cities:
+            if keyy[v] > 0 and keyy[v] < minimum:
+                minimum = keyy[v]
+                vertex = v
+        keyy[vertex] = 0
+        temp = []
+        temp.append(vertex)
+        temp.append(parent[vertex])
+        MSTedges.append(temp)
+        for v in cities:
+            w = calculate_distance(cities[v], cities[vertex])
+            if keyy[v] > w and w != 0:
+                keyy[v] = w
+                parent[v] = vertex
+    
+    dfsTraversal = []
+    u = 0
+    finalPath = []
+    dfsTraversal.append(-1)
+    while len(dfsTraversal):
+        if u not in finalPath:
+            finalPath.append(u)
+        hasNeighbors = False
+        for i in MSTedges:
+            if u in i:
+                dfsTraversal.append(u)
+                hasNeighbors = True
+                index = i.index(u)
+                i[index] = -1
+                if index == 0:
+                    u = i[index + 1]
+                else:
+                    u = i[index - 1]
+                MSTedges.remove(i)
+                break
+        if hasNeighbors == False:
+            u = dfsTraversal.pop()
 
     newPath = []
     count = 0
     change = True
     
-    while change:
+    while change and count < 40:
         change = False
         count = count + 1
         
@@ -91,7 +135,7 @@ except IOError as e:
     print 'Error in opening file '+ fileName
     sys.exit(-1)
 
-f2 = open(outFile,'w')
+#f2 = open(outFile,'w')
 
 #print array_w_identifier
 d, tour = greedy_tsp(maps)
@@ -99,4 +143,4 @@ print d
 print tour
 print len(tour)==len(set(tour)) #just to check if we visited any city twice
 print len(tour)
-f2.close()
+#f2.close()
